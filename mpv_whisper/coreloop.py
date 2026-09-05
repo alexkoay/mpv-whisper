956a3476ddb39522bbf8e5b70eda4c73246b72e4
Alex Koay <alexkoay88@gmail.com>
2026-09-05 09:14:28 +0800

    Capture exceptions in the thread.

--- mpv_whisper/coreloop.py
+++ mpv_whisper/coreloop.py
@@ -15,38 +15,44 @@ def core_loop(
     language: Optional[str] = None,
     cancel: Optional[Event] = None,
 ):
-    cancel = cancel or Event()
+    try:
+        cancel = cancel or Event()
 
-    log = logging.getLogger("core_loop")
+        log = logging.getLogger("core_loop")
 
-    config = get_config()
+        config = get_config()
 
-    subtitle = SRTFile(config.subtitle.get_subtitle(path))
-    subtitle.clear()
-    yield subtitle
+        subtitle = SRTFile(config.subtitle.get_subtitle(path), clear=False)
+        subtitle.clear()
+        yield subtitle
 
-    log.info("whispering %s", path)
-    language = language or config.transcribe.language
+        log.info("whispering %s", path)
+        language = language or config.transcribe.language
 
-    for chunk, start in generate_chunks(path, position, config.transcribe.chunk_duration):
-        if cancel.is_set():
-            break
-        log.debug("working: %s", to_time_format(start))
-        begin = time.time()
-        segments, language = whisper_chunk(chunk, language)
-        log.debug("got segments")
-        with subtitle.open("a"):
-            for segment in segments:
-                subtitle.write(
-                    start + segment.start,
-                    start + segment.end,
-                    segment.text,
-                )
-                yield segment
-        duration = time.time() - begin
-        log.info("progress: %s (%ss)", to_time_format(start), f"{duration:.2f}")
-    else:
-        log.info("completed whisper for %s", path)
-        yield True
+        for chunk, start in generate_chunks(path, position, config.transcribe.chunk_duration):
+            if cancel.is_set():
+                break
+            log.debug("working: %s", to_time_format(start))
+            begin = time.time()
+            segments, language = whisper_chunk(chunk, language)
+            log.debug("got segments")
+            with subtitle.open("a"):
+                for segment in segments:
+                    subtitle.write(
+                        start + segment.start,
+                        start + segment.end,
+                        segment.text,
+                    )
+                    yield segment
+            duration = time.time() - begin
+            log.info("progress: %s (%ss)", to_time_format(start), f"{duration:.2f}")
+        else:
+            with subtitle.open("a"):
+                subtitle.complete()
+            log.info("completed whisper for %s", path)
+            yield True
 
-    log.info("ending whisper for %s", path)
+        log.info("ending whisper for %s", path)
+    except Exception as e:
+        print(e)
+        raise
